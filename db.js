72953010ba9603aa678074ad8c3f30844d07f393
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-27 09:43:56 +0800

    Name of collection

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
-const mongoURL = 'mongodb+srv://admin:admin123_@cluster0.buswz.mongodb.net/test'
+
+const mongoURL = 'mongodb+srv://admin:admin123_@cluster0.buswz.mongodb.net/sanctuary'
+
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
+
 const connection = mongoose.connection
+
 connection.on('error', () => {
     console.log('Mongodb connection failed')
 })
