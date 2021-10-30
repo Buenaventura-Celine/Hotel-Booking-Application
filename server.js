5ea8093bcd4e824da555793e3b28ab1822d674b9
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-30 08:45:05 +0800

    Importing the route in the server

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const app = express();
 
 const dbConfig = require('./db')
 
+const roomsRoute = require('./routes/roomsRoute')
+
+app.use('/api/rooms', roomsRoute)
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
