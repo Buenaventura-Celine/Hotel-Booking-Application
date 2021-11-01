d8bc5ada8848d683a528de0cf6408545ce2b8595
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-11-01 13:14:56 +0800

    Add homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <BrowserRouter>
+        <Route path="/home" exact component={Homescreen} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/screens/Homescreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homescreen() {
+    return (
+        <div>
+            <h1>Home Screen </h1>
+        </div>
+    )
+}
+
+export default Homescreen
